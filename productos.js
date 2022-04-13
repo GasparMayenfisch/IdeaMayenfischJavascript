388c2455d76d0e03b58e47010f3ddc77d032309b
GasparMayenfisch <gasparmayenfisch@yahoo.com.ar>
2022-04-13 01:27:22 -0300

    2

--- productos.js
+++ productos.js
@@ -1,12 +1,5 @@
 const carrito= []
 let entrada = prompt("por favor seleccionar los productos que desea")
-
-if (console.log(productos.includes(entrada))=true) {
-    carrito.push(entrada);
-}
-else if (console.log(productos.includes(entrada))=false) {
-            alert("por favor ingresar un producto valido")
-}
 const productos =[
             {nombre:'thiner industrial hydra 5l', precio:2000},
             {nombre:'desengrasante industrial', precio:1500},
@@ -16,3 +9,52 @@ const productos =[
             {nombre:'atornillador', precio:23000},
             {nombre:'lijadora', precio:27000}
 ]
+const indexproducto = productos.indexOf(entrada)
+const productosselec =productos.slice(indexproducto,indexproducto) 
+class Producto {
+        constructor (nombre,precio){
+            this.nombre= nombre
+            this.precio= precio
+            this.vendido=false
+        }
+
+}
+function chequeodestock() {
+            if (cantidad!=0) {
+                console.log("hay stock");
+                
+            } else {
+                console.log("no hay stock");
+            }
+}
+function recargomediodepago1 (){
+        return precio * 1.20
+}
+function recargomediodepago2 (){
+        return precio * 1.15
+}
+function recargomediodepago3 (){
+        return precio * 0.90
+}
+function recargoamericanexpress(carrito,recargomediodepago1) {
+    for (const el of carrito) {
+        recargomediodepago1(el)
+        
+    }
+    
+}
+function recargomastercard(carrito,recargomediodepago2) {
+    for (const el of carrito) {
+        recargomediodepago2(el)
+        
+    }
+    
+}
+function recargovisa(carrito,recargomediodepago3) {
+    for (const el of carrito) {
+        recargomediodepago3(el)
+        
+    }
+    
+}
+
